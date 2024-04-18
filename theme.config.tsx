74c9cb987d8faa83030e40062a58f828d7972ecf
nishida <42875682+nishidayoshikatsu@users.noreply.github.com>
2024-04-18 19:49:58 +0900

    :lipstick: いい感じにnharukaさんの情報を追加

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>PCSS4VRC</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/nHaruka-git',
   },
-  chat: {
+  /*chat: {
     link: 'https://discord.com',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',*/
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'PCSS4VRC',
   },
 }
 
